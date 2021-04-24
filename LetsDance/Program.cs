c104e01fc0d3b130630140ca11e9554721b7d64a
Sixtten610 <60339623+Sixtten610@users.noreply.github.com>
2021-04-25 00:01:50 +0200

    DanceDanceBaby

---
+++ LetsDance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetsDance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class DanceAction
+    {
+        static public Queue<DanceAction> danceActions = new Queue<DanceAction>();
+        string action;
+        static Random generator = new Random();
+
+        public DanceAction()
+        {
+            action = RandAction();
+
+            danceActions.ToArray();
+        }
+
+        private string RandAction()
+        {
+            int word = generator.Next(1,4);
+
+            switch (word)
+            {
+                case 1:
+                return "left";
+                
+                case 2:
+                return "right";
+
+                case 3:
+                return "up";
+
+                case 4:
+                return "down";
+            }
+
+            return "";
+        }
+
+
+    }
+}
+
